ae8e3e8e13db0e6adc6e8ab90b3231b7fe5afb13
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-03-10 18:38:47 -0500

    lighthouse training

---
+++ ageCalculator.js
@@ -0,0 +1,11 @@
+function ageCalculator( name,yearOfBirth,currentYear)
+{
+  let presentage = currentYear - yearOfBirth ;
+  return name + " is " + presentage + " years old." ;
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
+
+
+
+ 
